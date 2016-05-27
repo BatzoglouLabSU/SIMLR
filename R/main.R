aeddae643ab05cd61c02d583aafc643a20ed0802
danro9685 <daniele.ramazzotti@yahoo.com>
2016-05-27 15:57:27 -0700

    main to test network diffusione

--- R/main.R
+++ R/main.R
@@ -4,6 +4,6 @@ source("compute.multiple.kernel.R")
 res1 = multiple.kernel(multiple.kernel.data.reduced)
 
 # test network.diffusion.R
-load(file="multiple.kernel.data.reduced.RData")
+load(file="network.diffiusion.data.reduced.RData")
 source("network.diffusion.R")
-res2 = network.diffusion(multiple.kernel.data.reduced,K=10)
+res2 = network.diffusion(network.diffiusion.data.reduced,K=10)
