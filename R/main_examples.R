9c7b14b82a3dfb6a0999be6ab486d8214c259a26
danro9685 <daniele.ramazzotti@yahoo.com>
2016-06-01 12:24:53 -0700

    set a reproducible seed for the experiments

--- R/main_examples.R
+++ R/main_examples.R
@@ -22,13 +22,17 @@ load(file="./../data/Test_3_Pollen.RData")
 load(file="./../data/Test_4_Usoskin.RData")
 
 # test SIMLR.R on example 1
+set.seed(11111)
 res_example1 = SIMLR(X=Test_1_mECS$in_X,c=Test_1_mECS$n_clust)
 
 # test SIMLR.R on example 2
+set.seed(22222)
 res_example2 = SIMLR(X=Test_2_Kolod$in_X,c=Test_2_Kolod$n_clust)
 
 # test SIMLR.R on example 3
+set.seed(33333)
 res_example3 = SIMLR(X=Test_3_Pollen$in_X,c=Test_3_Pollen$n_clust)
 
 # test SIMLR.R on example 4
+set.seed(44444)
 res_example4 = SIMLR(X=Test_4_Usoskin$in_X,c=Test_4_Usoskin$n_clust)
