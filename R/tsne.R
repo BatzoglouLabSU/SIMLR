ee3bcf61db9c005aa80c3dfe269a51677f289ae5
danro9685 <daniele.ramazzotti@yahoo.com>
2016-06-08 12:10:26 -0700

    finalization of tsne

--- R/tsne.R
+++ R/tsne.R
@@ -1,33 +1,33 @@
-"tsne" <-
-function(X, initial_config = NULL, k = 2, max_iter = 1000, min_cost = 0, epoch_callback = NULL, epoch = 100) {
+"tsne" <- function(X, initial_config = NULL, k = 2, max_iter = 1000, min_cost = 0, epoch = 100) {
     
     cat("Performing t-SNE.\n")
     
-    momentum = .8
-    final_momentum = .8
+    momentum = 0.8
+    final_momentum = 0.8
     mom_switch_iter = 250
 
     epsilon = 500
-    min_gain = .01
+    min_gain = 0.01
     initial_P_gain = 4
     
     n = nrow(X)
 
-    eps = 2^(-52) # typical machine precision
+    eps = .Machine$double.eps
 
     if (!is.null(initial_config) && is.matrix(initial_config)) {         
-        if (nrow(initial_config) != n | ncol(initial_config) != k){
+        if (nrow(initial_config) != n | ncol(initial_config) != k) {
             stop('initial_config argument does not match necessary configuration for X')
         }
         ydata = initial_config
         initial_P_gain = 1
         
-    } else {
+    }
+    else {
         ydata = matrix(rnorm(k * n),n)
     }
     
     P = X
-    P = .5 * (P + t(P))
+    P = 0.5 * (P + t(P))
 
     P[P < eps]<-eps
     P = P/sum(P)
@@ -39,27 +39,31 @@ function(X, initial_config = NULL, k = 2, max_iter = 1000, min_cost = 0, epoch_c
     
     for (iter in 1:max_iter) {
         
-        if (iter %% epoch == 0) { # epoch
-            cost =  sum(apply(P * log((P+eps)/(Q+eps)),1,sum))
-            message("Epoch: Iteration #",iter," error is: ",cost)
-            if (cost < min_cost) break
-            if (!is.null(epoch_callback)) epoch_callback(ydata)
-
+        if (iter %% epoch == 0) {
+            cost = sum(apply(P * log((P+eps)/(Q+eps)),1,sum))
+            
+            cat("Epoch: Iteration #",iter," error is: ",cost,"\n")
+            
+            if (cost < min_cost) {
+                break
+            }
         }
         
-        sum_ydata = apply(ydata^2, 1, sum)
-        num =  1/(1 + sum_ydata + sweep(-2 * ydata %*% t(ydata),2, -t(sum_ydata))) 
+        sum_ydata = apply((ydata^2),1,sum)
+        num =  1/(1 + sum_ydata + sweep(-2*ydata %*% t(ydata),2,-t(sum_ydata)))
         diag(num) = 0
         Q = num / sum(num)
-        if (any(is.nan(num))) message ('NaN in grad. descent')
-        Q[Q < eps] = eps
-        stiffnesses = 4 * (P-Q) * num
-        for (i in 1:n){
-            grads[i,] = apply(sweep(-ydata, 2, -ydata[i,]) * stiffnesses[,i],2,sum)
+        
+        if (any(is.nan(num))) {
+            message ('NaN in grad. descent')
         }
         
-        gains = (gains + .2) * abs(sign(grads) != sign(incs)) 
-                + gains * .8 * abs(sign(grads) == sign(incs))        
+        Q[Q < eps] = eps
+        
+        stiffnesses = (P-Q) * num
+        grads = 4 * (diag(apply(stiffnesses,2,sum)) - stiffnesses) %*% ydata
+        
+        gains = (gains + .2) * abs(sign(grads) != sign(incs)) + gains * .8 * abs(sign(grads) == sign(incs))
         gains[gains < min_gain] = min_gain
 
         incs = momentum * incs - epsilon * (gains * grads)
@@ -67,15 +71,19 @@ function(X, initial_config = NULL, k = 2, max_iter = 1000, min_cost = 0, epoch_c
         ydata = sweep(ydata,2,apply(ydata,2,mean))
         
         # we are constraining the ydata
-        ydata[ydata<-100] = -100
-        ydata[ydata>100] = 100
+        ydata[ydata < -100] = -100
+        ydata[ydata > 100] = 100
         
-        if (iter == mom_switch_iter) momentum = final_momentum
+        if (iter == mom_switch_iter) {
+            momentum = final_momentum
+        }
         
-        if (iter == 100 && is.null(initial_config)) P = P/4
+        if (iter == 100 && is.null(initial_config)) {
+            P = P/4
+        }
         
     }
     
-    ydata
+    return(ydata)
     
 }
--- main_examples.R
+++ main_examples.R
@@ -5,13 +5,15 @@ library(parallel)
 # load the igraph package to compute the NMI
 library(igraph)
 
+# load the palettes for the plots
+library(grDevices)
+
 # load the SIMLR R package
 source("./R/SIMLR.R")
 source("./R/compute.multiple.kernel.R")
 source("./R/network.diffusion.R")
 source("./R/utils.simlr.R")
 source("./R/tsne.R")
-source("./R/tsne-internal.R")
 
 # load the C file
 dyn.load("./src/projsplx_R.so")
@@ -41,3 +43,12 @@ nmi_3 = compare(Test_3_Pollen$true_labs[,1],res_example3$y$cluster,method="nmi")
 set.seed(44444)
 res_example4 = SIMLR(X=Test_4_Usoskin$in_X,c=Test_4_Usoskin$n_clust)
 nmi_4 = compare(Test_4_Usoskin$true_labs[,1],res_example4$y$cluster,method="nmi")
+
+# make the scatterd plots
+plot(Test_1_mECS$results$ydata,col=c(topo.colors(Test_1_mECS$n_clust))[Test_1_mECS$results$y$cluster],xlab="SIMLR component 1", ylab="SIMLR component 2",pch=20,main="SIMILR 2D visualization for Test_1_mECS")
+
+plot(Test_2_Kolod$results$ydata,col=c(topo.colors(Test_2_Kolod$n_clust))[Test_2_Kolod$results$y$cluster],xlab="SIMLR component 1", ylab="SIMLR component 2",pch=20,main="SIMILR 2D visualization for Test_2_Kolod")
+
+plot(Test_3_Pollen$results$ydata,col=c(topo.colors(Test_3_Pollen$n_clust))[Test_3_Pollen$results$y$cluster],xlab="SIMLR component 1", ylab="SIMLR component 2",pch=20,main="SIMILR 2D visualization for Test_3_Pollen")
+
+plot(Test_4_Usoskin$results$ydata,col=c(topo.colors(Test_4_Usoskin$n_clust))[Test_4_Usoskin$results$y$cluster],xlab="SIMLR component 1", ylab="SIMLR component 2",pch=20,main="SIMILR 2D visualization for Test_4_Usoskin")
