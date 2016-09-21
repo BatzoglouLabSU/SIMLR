966921b63ca74d8280aafbc48146410ed05f0ab7
danro9685 <daniele.ramazzotti@yahoo.com>
2016-09-21 09:50:21 -0700

    fixing vignette

--- R/data.R
+++ R/data.R
@@ -1,9 +1,8 @@
 #' @name test
 #' @title test dataset
-#' @description This dataset a todo
+#' @description example dataset to test SIMRL from the work by Buettner, Florian, et al. 
 #' @docType data
 #' @usage data(test)
-#' @format todo data
-#' @source todo
-#' @author Daniele Ramazzotti
+#' @format gene expression measurements of individual cells
+#' @source Buettner, Florian, et al. "Computational analysis of cell-to-cell heterogeneity in single-cell RNA-sequencing data reveals hidden subpopulations of cells." Nature biotechnology 33.2 (2015): 155-160. 
 NULL
