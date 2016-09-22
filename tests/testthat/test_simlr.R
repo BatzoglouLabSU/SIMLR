43ce7c5eeaea109ddfc09ce6267782412eebbdf4
luca-dex <luca.desano@gmail.com>
2016-09-22 11:24:55 +0200

    vignette and tests updated

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -1,16 +1,21 @@
 library(igraph)
 set.seed(11111)
 example = SIMLR(X=test$in_X, c=test$n_clust)
-
+ranks = SIMLR_Feature_Ranking(A=test$results$S,X=test$in_X)
 
 context("SIMLR")
-test_that("structure of output", {
+test_that("structure of output is compliant", {
 	expect_equal(names(example), c("y", "S", "F", "ydata",
 		"alphaK", "execution.time", "converge", "LF"))
 })
 
-test_that("test2", {
+test_that("correct approximation of output", {
 	expect_equal(
 		round(compare(test$true_labs[,1], example$y$cluster, method="nmi"), 2), 
 		round(0.888298, 2))
+})
+
+context("SIMLR ranking")
+test_that("structure of output is compliant", {
+	expect_equal(names(ranks), c("pval", "aggR"))
 })
