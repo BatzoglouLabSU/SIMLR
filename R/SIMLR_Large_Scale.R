a679c7b22f47bea2cae35ffb87d2a79d54620a67
danro9685 <daniele.ramazzotti@yahoo.com>
2017-01-19 12:14:25 -0800

    setting up the large scale R code

---
+++ R/SIMLR_Large_Scale.R
@@ -0,0 +1,151 @@
+# perform the SIMLR clustering algorithm for large scale datasets
+"SIMLR_Large_Scale" <- function( X, c, k = 10, kk = 100, if.impute = FALSE, normalize = FALSE, cores.ratio = 1 ) {
+    
+    # check the if.impute parameter
+    if(if.impute == TRUE) {
+        X = t(X)
+        X_zeros = which(X==0,arr.ind=TRUE)
+        if(length(X_zeros)>0) {
+            R_zeros = as.vector(X_zeros[,"row"])
+            C_zeros = as.vector(X_zeros[,"col"])
+            ind = (C_zeros - 1) * nrow(X) + R_zeros
+            X[ind] = as.vector(colMeans(X))[C_zeros]
+        }
+        X = t(X)
+    }
+    
+    # check the normalize parameter
+    if(normalize == TRUE) {
+        X = t(X)
+        X = X - min(as.vector(X))
+        X = X / max(as.vector(X))
+        C_mean = as.vector(colMeans(X))
+        X = apply(X,MARGIN=1,FUN=function(x) return(x-C_mean))
+    }
+    
+    # start the clock to measure the execution time
+    ptm = proc.time()
+    
+    # set some parameters
+    NITER = 5
+    num = ncol(X)
+    r = -1
+    beta = 0.8
+    X = t(X)
+    
+    cat("Performing fast PCA.\n")
+    fast.pca_res = fast.pca(X,k=kk)
+    fast.pca_res = t(fast.pca_res)
+    
+    cat("Performing k-nearest neighbour search.\n")
+    nearest_neighbour_res = knn.search(fast.pca_res,k*3)
+    val = nearest_neighbour_res$val
+    ind = nearest_neighbour_res$ind
+    
+    cat("Computing the multiple Kernels.\n")
+    
+    # compute the kernels
+    D_Kernels = multiple.kernel_large_scale(val,ind,k)
+    
+    # set up some parameters
+    alphaK = 1 / rep(length(D_Kernels),length(D_Kernels))
+    distX = array(0,c(dim(D_Kernels[[1]])[1],dim(D_Kernels[[1]])[2]))
+    for (i in 1:length(D_Kernels)) {
+        distX = distX + D_Kernels[[i]]
+    }
+    distX = distX / length(D_Kernels)
+    
+    # sort distX for rows
+    res = apply(distX,MARGIN=1,FUN=function(x) return(sort(x,index.return = TRUE)))
+    distX1 = array(0,c(nrow(distX),ncol(distX)))
+    idx = array(0,c(nrow(distX),ncol(distX)))
+    for(i in 1:nrow(distX)) {
+        distX1[i,] = res[[i]]$x
+        idx[i,] = res[[i]]$ix
+    }
+    
+    di = distX1[,2:(k+2)]
+    rr = 0.5 * (k * di[,k+1] - apply(di[,1:k],MARGIN=1,FUN=sum))
+    
+    if(r<=0) {
+        r = mean(rr)
+    }
+    lambda = max(mean(rr),0)
+    S0 = max(max(distX)) - distX
+    
+    cat("Performing the iterative procedure ",NITER," times.\n")
+    
+    # compute dn
+    S0 = dn(S0,'ave')
+    
+    S0_sparse = sparseMatrix(i=as.vector(matrix(rep(1:nrow(ind),ncol(ind)),nrow=nrow(ind),ncol=ncol(ind))),j=as.vector(ind),x=S0,symmetric=TRUE)
+    eig_res = eigs_sym(S0_sparse,c) eig_large_scale(S0,ind,c)
+    F_eig = eig_res$vectors
+    eig_res = eig_res$values
+    
+    # perform the iterative procedure NITER times
+    for(iter in 1:NITER) {
+        
+        cat("Iteration: ",iter,"\n")
+        
+        #### TO FIX FOR LARGE SCALE
+        distf = L2_distance_large_scale(F_eig,ind)
+        b = idx[,2:dim(idx)[2]]
+        a = apply(array(0,c(num,ncol(b))),MARGIN=2,FUN=function(x){ x = 1:num })
+        inda = cbind(as.vector(a),as.vector(b))
+        ad = (distX[inda]+lambda*distf[inda])/2/r
+        dim(ad) = c(num,ncol(b))
+        
+        # call the c function for the optimization
+        c_input = -t(ad)
+        c_output = t(ad)
+        ad = t(.Call("projsplx_R",c_input,c_output))
+        
+        ad = dn(S0,'ave')
+        S0 = beta * S0 + (1 - beta) * ad
+        
+        S0_sparse = sparseMatrix(i=as.vector(matrix(rep(1:nrow(ind),ncol(ind)),nrow=nrow(ind),ncol=ncol(ind))),j=as.vector(ind),x=S0,symmetric=TRUE)
+        eig_res = eigs_sym(S0_sparse,c)
+        F_eig = eig_res$vectors
+        eig_res = eig_res$values
+        
+        for (i in 1:length(D_Kernels)) {
+            temp = (.Machine$double.eps+D_Kernels[[i]]) * (S+.Machine$double.eps)
+            DD[i] = mean(apply(temp,MARGIN=2,FUN=sum))
+        }
+        alphaK0 = umkl(DD)
+        alphaK0 = alphaK0 / sum(alphaK0)
+        alphaK = (1-beta) * alphaK + beta * alphaK0
+        alphaK = alphaK / sum(alphaK)
+        lambda = 1.5 * lambda
+        r = r / 1.01
+        
+        # compute Kbeta
+        distX = D_Kernels[[1]] * alphaK[1]
+        for (i in 2:length(D_Kernels)) {
+            distX = distX + as.matrix(D_Kernels[[i]]) * alphaK[i]
+        }
+        
+    }
+    
+    #### TO FIX FOR LARGE SCALE
+    #I = repmat([1:size(S0,1)]',1,size(S0,2))
+    [S0 = sparse(I(:), ind(:), S0(:), size(S0,1),size(S0,1))
+    
+    # compute the execution time
+    execution.time = proc.time() - ptm
+    
+    # create the structure with the results
+    results = list()
+    results[["y"]] = NULL
+    results[["S0"]] = S0
+    results[["F"]] = F_eig
+    results[["ydata"]] = NULL
+    results[["alphaK"]] = alphaK
+    results[["val"]] = val
+    results[["ind"]] = ind
+    results[["execution.time"]] = execution.time
+    
+    return(results)
+    
+}
---
+++ R/utils.simlr.large.scale.R
@@ -0,0 +1,68 @@
+# function to compute fast PCA
+"fast.pca" = function( x, k ) {
+    
+    # performs svds
+    res_svds = svds(x,k)
+    U = res_svds$u
+    S = res_svds$d
+    
+    # perform fast PCA
+    tmp = array(0,c(length(S),length(S)))
+    diag(tmp) = S
+    res = U %*% tmp
+    
+    return(res)
+    
+}
+
+# function to perform k-nearest neighbour search and distances
+"knn.search" = function( xx, n ) {
+    
+    # performs k-nearest neighbour search
+    ind = randomProjectionTreeSearch(xx,n,distance_method="Euclidean")
+    
+    # compute the distances from the results
+    indices = neighborsToVectors(ind)
+    val = distance(i=indices$i,j=indices$j,x=xx,distance_method="Euclidean")
+    
+    res = list()
+    res$val = t(matrix(abs(val),nrow=n,ncol=dim(xx)[2]))
+    res$ind = ind + 1
+    
+    return(res)
+    
+}
+
+# compute and returns the multiple kernel for large scale data
+"multiple.kernel_large_scale" = function( val, ind, kk = 20 ) {
+    
+    # compute some parameters from the kernels
+    sigma = seq(2,1,-0.25)
+    
+    # compute the combined kernels
+    allk = seq(from=ceiling(kk/2),to=ceiling(kk*1.5),by=ceiling(kk/10))
+    
+    D_Kernels = list()
+    val_fun=val
+    ind_fun=ind
+    allk_fun=allk
+    sigma_fun=sigma
+    KK_fun=0
+    for(l in 1:length(allk)) {
+        if(allk_fun[l]<(ncol(val_fun))) {
+            TT = apply(val_fun[,1:allk_fun[l]],MARGIN=1,FUN=mean) + .Machine$double.eps
+            TT0 = apply(array(0,c(ncol(val_fun))),MARGIN=1,FUN=function(x) {x=TT})
+            TT0 = (TT0 + TT[ind_fun])*0.5
+            for (j in 1:length(sigma_fun)) {
+                temp = dnorm(val_fun,0,sigma_fun[j]*TT0)
+                temptemp = temp[,1]
+                temp = (apply(array(0,c(ncol(val_fun))),MARGIN=1,FUN=function(x) {x=temptemp}) + temptemp[ind_fun]) * 0.5 - temp
+                KK_fun = KK_fun + 1
+                D_Kernels[[KK_fun]] = temp + .Machine$double.eps
+            }
+        }
+    }
+    
+    return(D_Kernels)
+    
+}
