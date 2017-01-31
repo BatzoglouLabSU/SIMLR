bb22d4910d67e831e863d6af21995c51850048a9
danro9685 <daniele.ramazzotti@yahoo.com>
2017-01-30 17:45:22 -0800

    finalizing large scale code

--- MATLAB/src/mex_top_eig_svd.m
+++
@@ -1,7 +0,0 @@
-function [U,d] = mex_top_eig_svd(S0, ind, K)
-
-I = repmat([1:size(S0,1)]',1,size(S0,2));
-S0 = sparse(I(:), ind(:), S0(:), size(S0,1),size(S0,1)) + sparse(ind(:), I(:), S0(:), size(S0,1),size(S0,1));
-[U, d, ~] = eigs(S0, K);
-d= sqrt(d);
-%U = U(:,1:K)*diag(sqrt(sum(U(:,1:K).^2)));
