ef552e47b9c3db8db21fb7cfce35a1524ca252da
danro9685 <daniele.ramazzotti@yahoo.com>
2017-02-06 16:41:31 -0800

    script to install the required R libraries

---
+++ install_R_libraries.R
@@ -0,0 +1,7 @@
+# install the required R packages for the demos
+install.packages("Rcpp",dependencies=TRUE)
+install.packages("Matrix",dependencies=TRUE)
+install.packages("pracma",dependencies=TRUE)
+install.packages("RcppAnnoy",dependencies=TRUE)
+install.packages("RSpectra",dependencies=TRUE)
+install.packages("igraph",dependencies=TRUE)
