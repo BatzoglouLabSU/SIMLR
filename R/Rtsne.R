3011e88bb2df4bcc8d01f46c65f62e1e56c53fb6
danro9685 <daniele.ramazzotti@yahoo.com>
2017-03-28 10:25:14 -0700

    minor fixes

--- R/Rtsne.R
+++ R/Rtsne.R
@@ -1,6 +1,4 @@
 # Barnes-Hut implementation of t-Distributed Stochastic Neighbor Embedding modified for SIMLR large scale 
 Rtsne <- function( I, J, V, dims = 2, perplexity = 30, theta = 0.5, verbose = FALSE, max_iter = 300, Y_init = matrix(), init=FALSE, ... ) {
-  
   return(.Rtsne_cpp(I,J,V,dims,perplexity,theta,verbose,max_iter,Y_init,init))
-
 }
