cd8c07197e0318e773d54c37be8f1a8ab33fbae3
luca-dex <luca.desano@gmail.com>
2017-09-16 12:33:55 +0300

    remove test sceSET

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -27,18 +27,3 @@ test_that("structure of output is compliant", {
     expect_equal(names(normal_large_scale), c("y", "S0", "F", "ydata",
         "alphaK", "val", "ind", "execution.time"))
 })
-
-library(scran)
-ncells = 100
-ngenes = 50
-mu <- 2^runif(ngenes, 3, 10)
-gene.counts <- matrix(rnbinom(ngenes*ncells, mu=mu, size=2), nrow=ngenes)
-rownames(gene.counts) = paste0("X", seq_len(ngenes))
-sce = newSCESet(countData=data.frame(gene.counts))
-output = SIMLR(X = sce, c = 8, cores.ratio = 0)
-
-context("SIMRL SCESet")
-test_that("structure of output is compliant", {
-    expect_equal(names(output), c("y", "S", "F", "ydata",
-        "alphaK", "execution.time", "converge", "LF"))
-})
