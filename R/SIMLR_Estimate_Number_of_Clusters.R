2f4a989deae8452cb88c6054fcedda8d511f18bb
danro9685 <daniele.ramazzotti@yahoo.com>
2017-09-21 19:55:15 -0700

    bug fixing to num cluster estimation

--- R/SIMLR_Estimate_Number_of_Clusters.R
+++ R/SIMLR_Estimate_Number_of_Clusters.R
@@ -1,14 +1,14 @@
 # Estimates the number of clusters by means of two huristics
 "SIMLR_Estimate_Number_of_Clusters" = function( X, NUMC = 2:5, cores.ratio = 1 ) {
 
-    D_Kernels = multiple.kernel(t(X),cores.ratio)
+    D_Kernels = multiple.kernel.numc(t(X),cores.ratio)
     distX = array(0,c(dim(D_Kernels[[1]])[1],dim(D_Kernels[[1]])[2]))
     for (i in 1:length(D_Kernels)) {
         distX = distX + D_Kernels[[i]]
     }
     distX = distX / length(D_Kernels)
     W =  max(max(distX)) - distX
-    W = network.diffusion(W,max(ceiling(ncol(X)/20),10))
+    W = network.diffusion.numc(W,max(ceiling(ncol(X)/20),10))
 
     Quality = Estimate_Number_of_Clusters_given_graph(W,NUMC)
     Quality_plus = Estimate_Number_of_Clusters_given_graph(W,NUMC+1)
@@ -138,4 +138,130 @@
 
     return(Y)
 
+}
+
+# compute and returns the multiple kernel
+"multiple.kernel.numc" = function( x, cores.ratio = 1 ) {
+    
+    # set the parameters
+    kernel.type = list()
+    kernel.type[1] = list("poly")
+    kernel.params = list()
+    kernel.params[1] = list(0)
+    
+    # compute some parameters from the kernels
+    N = dim(x)[1]
+    KK = 0
+    sigma = seq(2,1,-0.25)
+    
+    # compute and sort Diff
+    Diff = dist2(x)
+    Diff_sort = t(apply(Diff,MARGIN=2,FUN=sort))
+    
+    # compute the combined kernels
+    m = dim(Diff)[1]
+    n = dim(Diff)[2]
+    allk = seq(10,30,2)
+    
+    # setup a parallelized estimation of the kernels
+    cores = as.integer(cores.ratio * (detectCores() - 1))
+    if (cores < 1 || is.na(cores) || is.null(cores)) {
+        cores = 1
+    }
+
+    cl = makeCluster(cores)
+    
+    clusterEvalQ(cl, {library(Matrix)})
+    
+    D_Kernels = list()
+    D_Kernels = unlist(parLapply(cl,1:length(allk),fun=function(l,x_fun=x,Diff_sort_fun=Diff_sort,allk_fun=allk,
+                                                                Diff_fun=Diff,sigma_fun=sigma,KK_fun=KK) {
+        if(allk_fun[l]<(nrow(x_fun)-1)) {
+            TT = apply(Diff_sort_fun[,2:(allk_fun[l]+1)],MARGIN=1,FUN=mean) + .Machine$double.eps
+            TT = matrix(data = TT, nrow = length(TT), ncol = 1)
+            Sig = apply(array(0,c(nrow(TT),ncol(TT))),MARGIN=1,FUN=function(x) {x=TT[,1]})
+            Sig = Sig + t(Sig)
+            Sig = Sig / 2
+            Sig_valid = array(0,c(nrow(Sig),ncol(Sig)))
+            Sig_valid[which(Sig > .Machine$double.eps,arr.ind=TRUE)] = 1
+            Sig = Sig * Sig_valid + .Machine$double.eps
+            for (j in 1:length(sigma_fun)) {
+                W = dnorm(Diff_fun,0,sigma_fun[j]*Sig)
+                D_Kernels[[KK_fun+l+j]] = Matrix((W + t(W)) / 2, sparse=TRUE, doDiag=FALSE)
+            }
+            return(D_Kernels)
+        }
+    }))
+    
+    stopCluster(cl)
+    
+    # compute D_Kernels
+    for (i in 1:length(D_Kernels)) {
+        K = D_Kernels[[i]]
+        k = 1/sqrt(diag(K)+.Machine$double.eps)
+        G = K * (k %*% t(k))
+        G1 = apply(array(0,c(length(diag(G)),length(diag(G)))),MARGIN=2,FUN=function(x) {x=diag(G)})
+        G2 = t(G1)
+        D_Kernels_tmp = (G1 + G2 - 2*G)/2
+        D_Kernels_tmp = D_Kernels_tmp - diag(diag(D_Kernels_tmp))
+        D_Kernels[[i]] = Matrix(D_Kernels_tmp, sparse=TRUE, doDiag=FALSE)
+    }
+    
+    return(D_Kernels)
+    
+}
+
+# perform network diffusion of K steps over the network A
+"network.diffusion.numc" <- function( A, K ) {
+    
+    # set the values of the diagonal of A to 0
+    diag(A) = 0
+    
+    # compute the sign matrix of A
+    sign_A = A
+    sign_A[which(A>0,arr.ind=TRUE)] = 1
+    sign_A[which(A<0,arr.ind=TRUE)] = -1
+    
+    # compute the dominate set for A and K
+    P = dominate.set(abs(A),min(K,nrow(A)-1)) * sign_A
+    
+    # sum the absolute value of each row of P
+    DD = apply(abs(P),MARGIN=1,FUN=sum)
+    
+    # set DD+1 to the diagonal of P
+    diag(P) = DD + 1
+    
+    # compute the transition field of P
+    P = transition.fields(P)
+    
+    # compute the eigenvalues and eigenvectors of P
+    eigen_P = eigen(P)
+    U = eigen_P$vectors
+    D = eigen_P$values
+    
+    # set to d the real part of the diagonal of D
+    d = Re(D + .Machine$double.eps)
+    
+    # perform the diffusion
+    alpha = 0.8
+    beta = 2
+    d = ((1-alpha)*d)/(1-alpha*d^beta)
+    
+    # set to D the real part of the diagonal of d
+    D = array(0,c(length(Re(d)),length(Re(d))))
+    diag(D) = Re(d)
+    
+    # finally compute W
+    
+    W = U %*% D %*% t(U)
+    diagonal_matrix = array(0,c(nrow(W),ncol(W)))
+    diag(diagonal_matrix) = 1
+    W = (W * (1-diagonal_matrix)) / apply(array(0,c(nrow(W),ncol(W))),MARGIN=2,FUN=function(x) {x=(1-diag(W))})
+    diag(D) = diag(D)[length(diag(D)):1]
+    W = (W + t(W)) / 2
+    
+    W[which(W<0,arr.ind=TRUE)] = 0
+    
+    return(W)
+    
 }
