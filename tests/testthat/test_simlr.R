893a67b1ad4001f2b2cb6772778fa31703585b2c
danro9685 <daniele.ramazzotti@yahoo.com>
2018-03-09 16:34:58 -0800

    updating tests

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -1,6 +1,7 @@
 normal = SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0)
 if.impute = SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, if.impute = TRUE)
 normalise = SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, normalize = TRUE)
+cimlr = CIMLR(X = GliomasReduced$in_X, c = 3, cores.ratio = 0)
 ranks = SIMLR_Feature_Ranking(A = BuettnerFlorian$results$S, X = BuettnerFlorian$in_X)
 
 context("SIMLR")
@@ -10,6 +11,12 @@ test_that("structure of output is compliant", {
     expect_equal(names(if.impute), c("y", "S", "F", "ydata",
         "alphaK", "execution.time", "converge", "LF"))
     expect_equal(names(normalise), c("y", "S", "F", "ydata",
+        "alphaK", "execution.time", "converge", "LF"))
+})
+
+context("CIMLR")
+test_that("structure of output is compliant", {
+    expect_equal(names(cimlr), c("y", "S", "F", "ydata",
         "alphaK", "execution.time", "converge", "LF"))
 })
 
