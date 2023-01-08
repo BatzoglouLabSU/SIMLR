a812a6a1aa99a1cd8aef7ca82261008e977b70f7
danro9685 <daniele.ramazzotti@yahoo.com>
2023-01-08 11:51:15 +0100

    bug fix

--- src/tsne.cpp
+++ src/tsne.cpp
@@ -892,7 +892,7 @@ void TSNE::computeSquaredEuclideanDistance(double* X, int N, int D, double* DD) 
     }
     double a1 = -2.0;
     double a2 = 1.0;
-    dgemm_("T", "N", &N, &N, &D, &a1, X, &D, X, &D, &a2, DD, &N, &Nsigned FCONE FCONE);
+    dgemm_("T", "N", &N, &N, &D, &a1, X, &D, X, &D, &a2, DD, &N, &N FCONE FCONE);
     free(dataSums); dataSums = NULL;
 }
 
